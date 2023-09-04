41bbf85c4f654ad9dc28ed842361f1e6eff69315
gurunaresh <gnareshblaze@gmail.com>
2023-09-04 20:30:06 +0530

    Add files via upload

---
+++ spring-boot-library-design/src/main/java/com/greatlearning/library/SpringBootLibraryDesignApplication.java
@@ -0,0 +1,15 @@
+package com.greatlearning.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLibraryDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLibraryDesignApplication.class, args);
+		System.out.println("Hi");
+		System.out.println("Hello Devtools");
+	}
+
+}
